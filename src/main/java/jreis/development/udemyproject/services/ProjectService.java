dc9adcea04d4e47d15738bac62fcf4eed9c7b868
João Robson <jreisam@gmail.com>
2018-10-30 14:50:56 -0400

    Endpoint FindByIdentifier e ajustes de exceptions.

--- src/main/java/jreis/development/udemyproject/services/ProjectService.java
+++ src/main/java/jreis/development/udemyproject/services/ProjectService.java
@@ -1,6 +1,7 @@
 package jreis.development.udemyproject.services;
 
 import jreis.development.udemyproject.domain.Project;
+import jreis.development.udemyproject.exceptions.ProjectIdException;
 import jreis.development.udemyproject.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,20 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        // logic
-        return projectRepository.save(project);
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Projeto ID: '" + project.getProjectIdentifier().toUpperCase() + "' já existe.");
+        }
+    }
+
+    public Project findProjectByIdentifier(String projectIdent){
+        Project project = projectRepository.findByProjectIdentifier(projectIdent.toUpperCase());
+        if(project == null)
+            throw new ProjectIdException("Projeto ID: '" + projectIdent.toUpperCase() + "' não existe.");
+
+        return project;
     }
 }
